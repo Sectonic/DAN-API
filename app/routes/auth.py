716601e0bbbcfa00a9d1b0b120ce72ce913572b4
Sectonic <hello@sujaldhakal.dev>
2025-03-25 20:24:45 -0400

    google redirect for mobile and desktop

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -7,8 +7,12 @@ bp = Blueprint("auth", __name__)
 @bp.route("/google", methods=["GET"])
 def google():
     code = request.args.get("code")
+    state = request.args.get("state")
     if not code:
         return jsonify({"error": "Code is required"}), 400
+    if state not in ["mobile", "desktop"]:
+        return jsonify({"error": "Invalid or missing state"}), 400
+    redirect_path = "exp://10.91.84.194:8081" if state is "mobile" else "http://localhost:8081"
     
     token_url = "https://oauth2.googleapis.com/token"
     data = {
@@ -26,7 +30,7 @@ def google():
         return jsonify({'error': 'ID token missing'}), 400
 
     try:
-        return f'<script>window.location.replace("exp://10.91.84.194:808/auth?id_token={id_token}")</script>', 200, {'Content-Type': 'text/html'}
+        return f'<script>window.location.replace("{redirect_path}/auth?id_token={id_token}")</script>', 200, {'Content-Type': 'text/html'}
     except Exception as e:
         return jsonify({"error": "Invalid token", "details": str(e)}), 400
     
