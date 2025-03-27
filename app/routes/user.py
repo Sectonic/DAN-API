9de6c092571cb8e8a9e1ca736a757a5ffb8aa9d7
Sectonic <hello@sujaldhakal.dev>
2025-03-27 16:19:43 -0400

    fixed user uuid response

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,7 +8,7 @@ bp = Blueprint('user', __name__)
 def get_caregiver_user(uid):
     try:
         caregiver = get_if_caregiver(get_user(uid))
-        return jsonify({ "contains": True }), 200 if caregiver else jsonify({ "contains": False}), 400
+        return (jsonify({ "contains": True }), 200) if caregiver else (jsonify({ "contains": False}), 400)
     except auth.UserNotFoundError:
         return jsonify({"contains": False}), 404
     except Exception as e:
